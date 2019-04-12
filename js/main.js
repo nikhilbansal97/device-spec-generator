0d0c871c5eb9edb1adbfd0f3de7106b0aefd263b
Nikhil Bansal <nikhil97.nb@gmail.com>
2019-04-12 21:00:22 +0530

    Add: Dynamic addition of checkboxes for ABI, screen density, SDKs and locale

--- js/main.js
+++ js/main.js
@@ -2,34 +2,67 @@ var abiList = ["armeabi", "armeabi-v7a", "arm64-v8a", "x86", "x86_64", "mips", "
 var localeList = ["en", "fr"];
 var screenDensityList = [320, 640];
 var sdkList = [22, 23, 24, 25, 26, 27, 28];
+var devicesList = ["One Plus Six", "Samsung S10"]
 
 var selectedAbi = ""
 var selectedLocale = ""
 var selectedScreenDensity = 0
 var selectedSdk = 0
 
+function initDropDown() {
+  devicesList.forEach((device) => {
+    $('<a class="dropdown-item device-item"></a>').html(device).appendTo(".device-dropdown")
+  })
+
+  abiList.forEach((abi) => {
+    $(".abi-container").append(`<div class=\"input-group mb-3\">\
+    <div class=\"input-group-prepend\">\
+      <div class=\"input-group-text\">\
+        <input type=\"checkbox\" aria-label=\"Checkbox for following text input\">\
+      </div>\
+    </div>\
+    <label class=\"form-control\">${abi}</label>\
+  </div>`)
+  })
+
+  localeList.forEach((locale) => {
+    $(".locale-container").append(`<div class=\"input-group mb-3\">\
+    <div class=\"input-group-prepend\">\
+      <div class=\"input-group-text\">\
+        <input type=\"checkbox\" aria-label=\"Checkbox for following text input\">\
+      </div>\
+    </div>\
+    <label class=\"form-control\">${locale}</label>\
+  </div>`)
+  })
+
+  screenDensityList.forEach((density) => {
+    $(".density-container").append(`<div class=\"input-group mb-3\">\
+    <div class=\"input-group-prepend\">\
+      <div class=\"input-group-text\">\
+        <input type=\"checkbox\" aria-label=\"Checkbox for following text input\">\
+      </div>\
+    </div>\
+    <label class=\"form-control\">${density}</label>\
+  </div>`)
+  })
+
+  sdkList.forEach((sdk) => {
+    $(".sdk-container").append(`<div class=\"input-group mb-3\">\
+    <div class=\"input-group-prepend\">\
+      <div class=\"input-group-text\">\
+        <input type=\"checkbox\" aria-label=\"Checkbox for following text input\">\
+      </div>\
+    </div>\
+    <label class=\"form-control\">${sdk}</label>\
+  </div>`)
+  })
+}
+
 function addListeners() {
-  $('.abi-item').click(function () {
+  $('.device-item').click(function () {
     selectedAbi = $(this).text()
-    $('#abiValue').text(selectedAbi)
-    generateJSON()
-  })
-
-  $('.locale-item').click(function () {
-    selectedLocale = $(this).text()
-    $('#localeValue').text(selectedLocale)
-    generateJSON()
-  })
-
-  $('.screen-item').click(function () {
-    selectedScreenDensity = $(this).text()
-    $('#screenValue').text(selectedScreenDensity)
-    generateJSON()
-  })
-
-  $('.sdk-item').click(function () {
-    selectedSdk = $(this).text()
-    $('#sdkValue').text(selectedSdk)
+    $('#deviceValue').text(selectedAbi)
     generateJSON()
   })
 }
@@ -48,4 +81,5 @@ function isInfoValid() {
     selectedScreenDensity != 0
 }
 
+initDropDown()
 addListeners()
